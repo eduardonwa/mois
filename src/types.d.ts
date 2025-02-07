b93b436bcd45b2e06091b45d1b1596b3485b9c90
eduardonwa <eduardongua@hotmail.com>
2025-02-07 10:59:16 -0700

    me caga typescript

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,86 +1,155 @@
-import type { arrayOutputType } from "astro:schema";
+import type { array, arrayOutputType } from "astro:schema";
 
 // Definición base para enlaces
 interface BaseLink {
-    linkType: "href" | "page" | "post";
-    href?: string;
-    pageSlug?: string;
-    postSlug?: string;
-    openInNewTab?: boolean;
-    [key: string]: any;
+  linkType: "href" | "page" | "post";
+  href?: string;
+  pageSlug?: string;
+  postSlug?: string;
+  openInNewTab?: boolean;
+  [key: string]: any;
 }
 
-// Para la barra de navegación (navbar.astro)
+// Para la barra de navegación
 interface NavbarItem {
-    name: string;
-    label: string;
-    link: BaseLink & {
-        slug: string;
-    };
+  name: string;
+  label: string;
+  link: BaseLink & { slug: string };
 }
 
-// Para el pie de página (footer.astro)
+// Para el pie de página
 interface FooterItem {
-    logoUrl?: string;
-    linkGroups?: LinkGroup[];
+  logoUrl?: string;
+  linkGroups?: LinkGroup[];
 }
 
 interface LinkGroup {
-    groupTitle: string;
-    links?: Link[];
+  groupTitle: string;
+  links?: Link[];
 }
 
-interface Link {
-    name: string;
-    link: BaseLink;
+// Interfaz unificada para Link
+interface Link extends BaseLink {
+  name?: string;
 }
 
 // Para atributos de enlace HTML
 export interface LinkAttributes {
-    target?: string;
-    rel?: string;
-    href: string;
-    class?: string;
-    [key: string]: string | undefined;
+  target?: string;
+  rel?: string;
+  href: string;
+  class?: string;
+  [key: string]: string | undefined;
 }
 
-// Props para el componente Link (link.astro)
-interface LinkProps {
-    link: BaseLink;
-    class?: string;
-}
-
+// Para los campos de metadata
 export interface MetadataSettingsProps {
   title: string;
   description: string;
   ogImage?: {
     alt: string;
     baseUrl: string;
-    ogImageUrl: string;
+    ogImageUrl: SanityAsset;
+  };
+}
+
+export interface SanityAsset {
+  _type: string;
+  asset: {
+    _ref: string;
+    _type: string;
   };
 }
 
 export interface PageSlugProps {
   heading: string;
   subheading: string;
-  pageBuilder: array;
+  pageBuilder: BlockComponent[];
 }
 
-// Interfaz base para todos los bloques
+interface BlockComponents {
+  callToAction: (props: CallToActionProps) => any;
+  infoSection: (props: InfoSectionProps) => any;
+  link: (props: LinkProps) => any;
+  image: (props: ImageProps) => any; // Añade el tipo para Image
+  block: (props: BlockProps) => any; // Añade el tipo para Block
+}
+
+export const blockComponents: BlockComponents = {
+  callToAction: CallToAction,
+  infoSection: InfoSection,
+  link: Link,
+  image: Image, // Incluye el componente Image
+  block: Block, // Incluye el componente Block
+};
+
+interface BlockComponent extends BlockBase {
+  _type: "callToAction" | "infoSection" | "image" | "block";
+  callToAction?: CallToActionProps;
+  infoSection?: InfoSectionProps;
+  image?: Image;
+  block?: Block;
+}
+
 interface BlockBase {
   _type: string;
   _key?: string;
   _id?: string;
 }
 
-interface CallToActionProps {
-
+interface BlockContent {
+  _type: "array";
+  of: BlockComponent[];
 }
 
-interface InfoSectionProps {
-
+interface Block extends BlockBase {
+  _type: "block";
+  style: "normal" | "h1" | "h2" | "h3" | "h4" | "blockquote";
+  lists?: { _type: "list"; style: "bullet"; children: ListItem[] }[];
+  children: Span[];
+  markDefs: Link[];
 }
 
+interface Span {
+  _type: "span"; // _type en minúsculas
+  text: string;
+  marks: string[];
+}
+
+interface ListItem {
+  _type: "listItem";
+  children: Span[];
+}
+
+interface Image extends BlockBase {
+  _type: "image";
+  asset: {
+    _ref: string;
+    _type: string;
+  };
+  alt?: string;
+  hotspot?: boolean;
+}
+
+// (components/CallToAction.astro)
+interface CallToActionProps extends BlockBase {
+  _type: "callToAction";
+  heading?: string;
+  subheading?: string;
+  buttonText?: string;
+  link?: Link;
+}
+
+// (components/InfoSection.astro)
+interface InfoSectionProps extends BlockBase {
+  _type: "infoSection";
+  heading?: string;
+  subheading?: string;
+  content?: BlockContent;
+}
+
+// (components/Link.astro)
 interface LinkProps {
-  
+  link: BaseLink;
+  class?: string;
 }
