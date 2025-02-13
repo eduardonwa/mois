d38b0987c97712705a45cc89439c21aaab7bbd56
eduardonwa <eduardongua@hotmail.com>
2025-02-12 23:25:44 -0700

    - arregle un error en donde no todas las páginas recibian el draftMode ni visual editing enabled a true. - corregi un error de scss

--- src/lib/draftMode.ts
+++ src/lib/draftMode.ts
@@ -1,4 +1,13 @@
-export function isDraftMode(request: Request): boolean {
+function isDraftMode(request: Request): boolean {
     const cookie = request.headers.get('cookie');
     return cookie?.includes('sanity_preview=true') ?? false;
+}
+
+export function getDraftModeInfo(request: Request) {
+  const serverDraftMode = isDraftMode(request);
+  const visualEditingEnabled = import.meta.env.PUBLIC_SANITY_VISUAL_EDITING_ENABLED === "true";
+  
+  //console.log('getDraftModeInfo:', { serverDraftMode, visualEditingEnabled });
+  
+  return { serverDraftMode, visualEditingEnabled };
 }
